5ff3ecafb71b2f462f73bcbd9cf85f00f3e8d7f2
Vasilii Besschetnov <vbesschetnov@yandex.ru>
2018-01-02 15:46:19 -0800

    css modules are used

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import { NavLink  } from "react-router-dom";
-import "./Header.css";
+import styles from "./Header.css";
+
+const createLink = (text, path = "/")=> {
+    return (
+        <NavLink key={path} to={path}
+            exact={true}
+            className={styles.link}
+            activeClassName={styles.active}>
+            {text}
+        </NavLink>
+    );
+}
 
 const Header = (props) => {
+    const links = [{
+        text: "Home"
+    },
+    {
+        text: "Stores",
+        path: "/Stores"
+    },
+    {
+        text: "Categories",
+        path: "/Categories"
+    }];
+    
     return (
-        <div>            
-            <NavLink to="/"
-                exact={true}
-                className="link"
-                activeClassName="active">
-                Home
-            </NavLink>
-            <NavLink to="/Categories"
-                activeClassName="active">
-                Categories
-            </NavLink>
+        <div>
+            {links.map(l => createLink(l.text, l.path))}
         </div>
     );
 };
